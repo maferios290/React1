3049f1e0f02522cd7d34d84fe7a72ee091d871d9
maferios290 <maferios290@gmail.com>
2025-04-01 20:45:59 -0500

    cambio1
    cambio1

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; //arbol del html
+
+//se raliza un import de un archivo llamado APP que se comporta como un componente
+import App from '../../React2/src/App';
+
+//muestreme el contenido del App y rindirice la pagina
+ReactDOM.render(<App />,
+document.getElementById('root'));
