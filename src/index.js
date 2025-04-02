6c8d90fd4ca58dd095d44b58583f173d4c240423
maferios290 <maferios290@gmail.com>
2025-04-01 21:03:01 -0500

    2
    2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom'; //arbol del html
 
 //se raliza un import de un archivo llamado APP que se comporta como un componente
-import App from '../../React2/src/App';
+import App from './App';
 
 //muestreme el contenido del App y rindirice la pagina
 ReactDOM.render(<App />,
